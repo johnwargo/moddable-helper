864a85c4ef8e32ac40ad81ce6b850ece690ccb42
John M. Wargo <johnwargo@users.noreply.github.com>
2021-05-04 18:12:52 -0400

    completed implementation of interactive Deployment

--- mddbl.js
+++ mddbl.js
@@ -1,5 +1,41 @@
 #!/usr/bin/env node
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var config_1 = require("./config");
 var chalk = require('chalk');
@@ -8,6 +44,7 @@ var logger = require('cli-logger');
 var os = require('os');
 var path = require('path');
 var program = require('commander');
+var Select = require('enquirer').Select;
 var cp = require("child_process");
 var packageDotJSON = require('./package.json');
 var APP_NAME = '\nModdable Helper (mddbl)';
@@ -92,7 +129,8 @@ function doDeploy(rootCmd, mod, target) {
 function deployModule(modName, targetName) {
     if (targetName === void 0) { targetName = ''; }
     log.debug("deployModule(" + modName + ", " + targetName + ")");
-    readConfig();
+    if (!appConfig)
+        readConfig();
     var mod = appConfig.modules.find(function (item) { return item.name === modName; });
     if (!mod) {
         log.error("Module '" + modName + "' not defined, " + CHECK_CONFIG_STRING);
@@ -128,7 +166,60 @@ function deployModule(modName, targetName) {
     }
 }
 function deployInteractive() {
-    log.info('Deploying in interactive mode');
+    return __awaiter(this, void 0, void 0, function () {
+        var modPrompt, targetPrompt, modName, targetName;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    log.debug('Deploying in interactive mode');
+                    readConfig();
+                    if (appConfig.modules.length < 1) {
+                        log.error("Module list not defined, " + CHECK_CONFIG_STRING);
+                        process.exit(1);
+                    }
+                    if (appConfig.targets.length < 1) {
+                        log.error("Module list not defined, " + CHECK_CONFIG_STRING);
+                        process.exit(1);
+                    }
+                    modPrompt = new Select({
+                        name: 'modName',
+                        message: 'Module Selection',
+                        choices: appConfig.modules
+                    });
+                    targetPrompt = new Select({
+                        name: 'targetName',
+                        message: 'Target Selection',
+                        choices: appConfig.targets
+                    });
+                    modName = '';
+                    targetName = '';
+                    return [4, modPrompt.run()
+                            .then(function (result) {
+                            modName = result.toString();
+                        })
+                            .catch(function (err) {
+                            log.error(err);
+                            process.exit(1);
+                        })];
+                case 1:
+                    _a.sent();
+                    return [4, targetPrompt.run()
+                            .then(function (result) {
+                            targetName = result.toString();
+                        })
+                            .catch(function (err) {
+                            log.error(err);
+                            process.exit(1);
+                        })];
+                case 2:
+                    _a.sent();
+                    if (modName.length > 0 && targetName.length > 0) {
+                        deployModule(modName, targetName);
+                    }
+                    return [2];
+            }
+        });
+    });
 }
 function wipeDevice(targetName) {
     log.debug("wipeDevice(" + targetName + ")");
@@ -272,14 +363,15 @@ program
     .description('Toggle the debug configuration setting')
     .action(toggleDebug);
 program
-    .command('deploy')
-    .description('Deploy; interactive mode')
-    .action(deployInteractive);
-program
-    .command('deploy <module> [target]')
-    .description('Deploy <module> to specified [target] device')
+    .command('deploy [module] [target]')
+    .description('Deploy Module to specified Target device')
     .action(function (mod, target) {
-    deployModule(mod, target);
+    if (mod) {
+        deployModule(mod, target);
+    }
+    else {
+        deployInteractive();
+    }
 });
 program
     .command('init')
